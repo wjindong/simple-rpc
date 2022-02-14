6dcc26c2d14af653055de20c91dcf468355ef0c9
王谨东 <jindongwang@qq.com>
2022-02-14 17:58:21 +0800

    新增性能测试

--- samples/src/main/java/com/simple/rpc/samples/client/Application.java
+++ samples/src/main/java/com/simple/rpc/samples/client/Application.java
@@ -5,6 +5,7 @@ import com.simple.rpc.client.async.AsyncClient;
 import com.simple.rpc.client.future.FutureResult;
 import com.simple.rpc.samples.api.GreetingsService;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class Application {
@@ -27,5 +28,33 @@ public class Application {
 
         System.out.println(res);
         System.out.println(error);
+
+        //性能测试
+        //1. 单次调用延迟
+        long startTime=System.currentTimeMillis();
+        service.sayHi("test");
+        long cost=System.currentTimeMillis()-startTime;
+        System.out.println("单次调用延迟："+cost+"ms");
+
+        //2. qps
+        int synCallTimes=20000,threadNum=30;
+        CountDownLatch latch=new CountDownLatch(threadNum);
+
+        startTime=System.currentTimeMillis();
+
+        for(int i=0;i<threadNum;i++){
+            new Thread(()->{
+                for(int j=0;j<synCallTimes;j++){
+                    service.sayHi(String.valueOf(j));
+                }
+                latch.countDown();
+            }).start();
+        }
+        latch.await();
+
+        cost=System.currentTimeMillis()-startTime;
+        System.out.println("qps: "+synCallTimes*threadNum/1.0/cost*1000.0);
+
+        client.stop();
     }
 }
